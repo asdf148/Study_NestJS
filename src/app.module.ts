32d3900b629fb06966ed98640cef6ca65ba82c32
qwer142 <wnduf0405_@naver.com>
2021-05-21 12:02:09 +0900

    post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
