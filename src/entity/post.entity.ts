90101d1a22a162a6912bb7ca62b57ad3b9ebda6c
asdf <wnduf0405_@naver.com>
2021-06-01 17:21:30 +0900

    update post.entity

--- src/entity/post.entity.ts
+++ src/entity/post.entity.ts
@@ -1,26 +1,34 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { User } from './user.entity';
 
 @Entity()
 export class EPost {
+  @IsOptional()
+  @IsNumber()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsString()
   @Column()
   title: string;
 
+  @IsString()
   @Column()
   content: string;
 
+  @IsNumber()
   @ManyToOne(
       () => User,
       (user) => user.posts
   )
   user: User;
 
+  @IsOptional()
   @CreateDateColumn()
   createdAt: Date;
 
+  @IsOptional()
   @UpdateDateColumn()
   updatedAt: Date;
 }
